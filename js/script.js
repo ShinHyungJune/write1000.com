7c634379fa823be665af50ed23330496df6b079f
ShinHyungJune <ssa4141@naver.com>
2024-08-28 00:58:53 +0900

    글천개 연구소 수정

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+window.domain = "https://preview-geul1000gaehome.olly.kr/api";
+window.key = "kh9cdcbhv6t57glbq0doy65xadhd0fs4";
+
 $(document).ready(function (){
     //헤더 푸터 컴포넌트
     $('#header').load('components/header.html');
